ab616f36d4d94e1181d963ee50977b3c53bfdd2e
Yonas <850021443@qq.com>
2025-11-27 23:19:50 +0800

    优化绘图过程。中间不再绘制tif，使用set_data更新绘图，确保在多block预测过程中能快速稳定更新中间视图

--- cnn_model/Predict.py
+++ cnn_model/Predict.py
@@ -1,5 +1,3 @@
-"""将大幅高光谱影像进行分块的滑窗预测，避免占用大量显存
-预测结果是一个二维矩阵，255代表背景, 其余值代表预测的地物类别, 最多预测256类（包括背景）"""
 import sys, os
 sys.path.append('.')
 from cnn_model.Models.Data import Predict_Dataset
@@ -19,11 +17,11 @@ matplotlib.use('Agg')
 output_path = 'SRACN_PRE.tif'
 batch = 128
 input_data = r"C:\Users\85002\OneDrive - cugb.edu.cn\项目数据\张川铀资源\ZY_result\Image\research_area1.dat"
-model_pth = r'cnn_model\_results\Common_1DCNN_Test_Patch17_202511241428_ID9iuaw3s6bopxyubfbtdog\Common_1DCNN_Test_Patch17_202511241428_ID9iuaw3s6bopxyubfbtdog_best.pt'  # 模型路径
+model_pth = r'cnn_model\_results\Common_1DCNN_Test_Patch17_202511272256_IDnkb8of1wa3ej9a11xj0hx\Common_1DCNN_Test_Patch17_202511272256_IDnkb8of1wa3ej9a11xj0hx_best.pt'  # 模型路径
 MUTITHREADING_MODE = False # 是否使用多线程加速数据加载, 实测Fasle时速度更快，根据情况使用
-DRAW_RGB = True # 是否绘制预测过程中的rgb图像
 rgb_combine = (25,15,5) #(29,19,9) # 绘制图像时的rgb组合，从1开始, 如果无效则使用第一个波段, 图像太大时一定程度上会影响速度
 image_block_size = 512 # 分块预测时每个大块的大小，越大越占用内存，但预测速度越快
+
 if __name__ == '__main__':
     patch_size = re.search(r'Patch(\d+)', os.path.basename(model_pth))
     if patch_size is None:
@@ -37,16 +35,20 @@ if __name__ == '__main__':
     img = Hyperspectral_Image()
     img.init(input_data, rgb=rgb_combine)
     predict_whole_map = np.empty((img.rows,img.cols), dtype=np.uint8) + 255 # 背景值为-1
-    model = torch.load(model_pth, weights_only=False, map_location=device)
-    model.to(device)
-    model.eval()
-    if DRAW_RGB:
-        out_png = f"{output_path[:-4]}.png"
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-        ax1.imshow(img.ori_img)
-        ax1.axis('off')
-        ax2.axis('off')
 
+    # 设置绘制图像的参数
+    out_png = f"{output_path[:-4]}.png"
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+    global_rgb_vis = utils.label_to_rgb(predict_whole_map, background_value=255) 
+    ax1.imshow(img.ori_img)
+    ax1.axis('off')
+    pred_imshow_obj = ax2.imshow(global_rgb_vis) # 获取 image object 引用，后续通过它更新数据
+    ax2.axis('off')
+    plt.tight_layout()
+
+    model = torch.load(model_pth, weights_only=False, map_location=device)
+    model.to(device) # 移动模型到指定设备
+    model.eval() # 设置为评估模式
     num_workers = cpu_count() // 4 if MUTITHREADING_MODE else 0
     dataset = Predict_Dataset(patch_size)
     dataloader = DataLoader(dataset, batch_size=batch, shuffle=False, num_workers=num_workers, 
@@ -55,11 +57,11 @@ if __name__ == '__main__':
         for image_block, background_mask, i, j in img.image_block_iter(block_size=image_block_size, patch_size=patch_size):
             rows, cols = background_mask.shape
             predict_map = np.zeros((rows, cols), dtype=np.uint8) + 255 # 初始化一个空的预测map，-1代表背景值
-            if np.any(background_mask == True): # 如果
+            if np.any(background_mask): # 如果
                 idx = 0
                 dataset.update_data(image_block, background_mask) # 更新dataset后调用dataloader会重新启动进程
                 predict_data = torch.empty(len(dataset), dtype=torch.uint8, device=device) # 预分配内存，用来储存预测结果
-                for data in tqdm(dataloader, total=len(dataloader), desc=f'Block Predicting'):
+                for data in tqdm(dataloader, total=len(dataloader), desc='Block Predicting'):
                     batch = data.shape[0]
                     data = data.to(device)
                     outputs = model(data)
@@ -68,10 +70,10 @@ if __name__ == '__main__':
                     idx += batch
                 predict_map[background_mask] = predict_data.cpu().numpy() if predict_data.device.type == 'cuda' else predict_data.numpy() # 将预测结果填入对应位置
                 predict_whole_map[i:i+rows, j:j+cols] = predict_map # 将预测结果填入整体预测矩阵
-                img.save_tif(output_path, predict_whole_map, nodata=255) # 保存为tif文件
 
-                # 下面保存预测过程中的图像
-                if DRAW_RGB:
-                    map = utils.label_to_rgb(predict_whole_map, background_value=255)
-                    ax2.imshow(map)
-                    fig.savefig(out_png, bbox_inches='tight', dpi=150)
+                # 更新预测过程中的图像
+                block_rgb = utils.label_to_rgb(predict_map, background_value=255)
+                global_rgb_vis[i:i+rows, j:j+cols] = block_rgb
+                pred_imshow_obj.set_data(global_rgb_vis)
+                fig.savefig(out_png, dpi=150)
+    img.save_tif(output_path, predict_whole_map, nodata=255) # 最终保存为tif文件
